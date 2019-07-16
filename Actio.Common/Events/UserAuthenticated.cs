92766b9a0719105b74cb948fc271b8a81465f486
Mariusz Krzysztof Przybysz <m.przybysz@storware.eu>
2019-07-16 23:37:46 +0200

    Creating events

---
+++ Actio.Common/Events/UserAuthenticated.cs
@@ -0,0 +1,17 @@
+﻿namespace Actio.Common.Events
+{
+    public class UserAuthenticated : IEvent
+    {
+        public UserAuthenticated(string email)
+        {
+            Email = email;
+        }
+
+        protected UserAuthenticated()
+        {
+
+        }
+
+        public string Email { get; }
+    }
+}
